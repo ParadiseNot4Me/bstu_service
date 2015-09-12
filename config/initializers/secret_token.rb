decf4aeb3fcd6b5c84d44fb50ae60d805777b4c3
Иван Григорьев <ig@poloniumarts.com>
2015-09-12 18:21:30 +0300

    init bstu service

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BstuService::Application.config.secret_key_base = '1da12550756f1d26a27fbe54bfb890266b95e01e12ca98072ab8471db686910814800e02775af6f004b42ed25dc8a42c703c01b90474ab3e5b1b258e775427e3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BstuService::Application.config.session_store :cookie_store, key: '_bstu_service_session'
