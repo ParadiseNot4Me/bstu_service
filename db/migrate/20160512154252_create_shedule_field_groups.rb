7116d61ba9f78e59be56869cc59763c194997c36
Илья Самородов <is@poloniumarts.com>
2016-05-12 23:22:24 +0300

    add| scdelule controller

--- db/migrate/20160512154252_create_shedule_field_groups.rb
+++ db/migrate/20160512154252_create_shedule_field_groups.rb
@@ -1,8 +1,8 @@
-class CreateSheduleFieldGroups < ActiveRecord::Migration
+class CreateScheduleFieldGroups < ActiveRecord::Migration
   def change
-    create_table :shedule_field_groups do |t|
+    create_table :schedule_field_groups do |t|
       t.references :group, index: true, foreign_key: true
-      t.references :schedule_fields, index: true, foreign_key: true
+      t.references :schedule_field, index: true, foreign_key: true
 
       t.timestamps null: false
     end
--- db/migrate/20160512161534_rename.rb
+++
@@ -1,5 +0,0 @@
-class Rename < ActiveRecord::Migration
-  def change
-  	  rename_table :shedule_field_groups, :schedule_field_groups
-  end
-end
